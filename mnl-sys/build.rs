4a60dd9296bffcafb89f330a204a5b55f1efd083
Linus Färnstrand <linus@mullvad.net>
2018-06-04 17:49:09 +0200

    Add build.rs check for LIBMNL_LIB_DIR and use to link

--- mnl-sys/build.rs
+++ mnl-sys/build.rs
@@ -1,5 +1,8 @@
 extern crate pkg_config;
 
+use std::env;
+use std::path::PathBuf;
+
 #[cfg(feature = "mnl-1-0-4")]
 const MIN_VERSION: &str = "1.0.4";
 #[cfg(not(feature = "mnl-1-0-4"))]
@@ -8,11 +11,23 @@ const MIN_VERSION: &str = "1.0.3";
 
 #[cfg(target_os = "linux")]
 fn main() {
-    println!("Minimum libmnl version: {}", MIN_VERSION);
-    pkg_config::Config::new()
-        .atleast_version(MIN_VERSION)
-        .probe("libmnl")
-        .unwrap();
+    if let Ok(lib_dir) = env::var("LIBMNL_LIB_DIR").map(PathBuf::from) {
+        if !lib_dir.is_dir() {
+            panic!(
+                "libmnl library directory does not exist: {}",
+                lib_dir.display()
+            );
+        }
+        println!("cargo:rustc-link-search=native={}", lib_dir.display());
+        println!("cargo:rustc-link-lib=mnl");
+    } else {
+        // Trying with pkg-config instead
+        println!("Minimum libmnl version: {}", MIN_VERSION);
+        pkg_config::Config::new()
+            .atleast_version(MIN_VERSION)
+            .probe("libmnl")
+            .unwrap();
+    }
 }
 
 #[cfg(not(target_os = "linux"))]
